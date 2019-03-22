77dfff13edc921a9793af57d4e1387d5b8617902
Sheldrick <mrmayrant@yahoo.com>
2019-03-22 18:54:59 -0400

    added the javascript necessary for the slider

---
+++ dist/js/main.js
@@ -0,0 +1,58 @@
+const slides = document.querySelectorAll(".slide");
+const next = document.querySelector("#next");
+const prev = document.querySelector("#prev");
+const auto = true;
+const intervalTime = 5000;
+let slideInterval;
+
+const nextSlide = () => {
+    const current = document.querySelector('.current');
+    // remove current class
+    current.classList.remove("current");
+    // check if there's a sibling slide element
+    if (current.nextElementSibling) {
+        // add current class to next sibling
+        current.nextElementSibling.classList.add("current")
+    } else {
+        // add current class to first slide
+        slides[0].classList.add("current")
+    }
+    setTimeout(() => current.classList.remove("current"));
+}
+
+const prevSlide = () => {
+    const current = document.querySelector('.current');
+    // remove current class
+    current.classList.remove("current");
+    // check if there's a sibling slide element
+    if (current.previousElementSibling) {
+        // add current class to previous sibling
+        current.previousElementSibling.classList.add("current")
+    } else {
+        // add current class to last slide
+        slides[slides.length - 1].classList.add("current")
+    }
+    setTimeout(() => current.classList.remove("current"));
+}
+
+next.addEventListener("click", e => {
+    nextSlide();
+    // reset the interval once the arrow has been clicked to move to next image
+    if (auto) {
+        clearInterval(slideInterval);
+        slideInterval = setInterval(nextSlide, intervalTime);
+    }
+});
+prev.addEventListener("click", e => {
+    prevSlide();
+    // reset the interval once the arrow has been clicked to move to prev image
+    if (auto) {
+        clearInterval(slideInterval);
+        slideInterval = setInterval(nextSlide, intervalTime);
+    }
+});
+
+if (auto) {
+    //  run next slide function at interval time
+    slideInterval = setInterval(nextSlide, intervalTime)
+}
