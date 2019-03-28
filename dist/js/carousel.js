95763b09e7109f083ab7be7be6a6431ca60717a4
Sheldrick <mrmayrant@yahoo.com>
2019-03-28 03:20:23 -0400

    added an noverlay to the carousel images

--- dist/js/carousel.js
+++ dist/js/carousel.js
@@ -5,16 +5,21 @@ let slideInterval;
 
 const nextSlide = () => {
     const current = document.querySelector('.current');
+    const darkenImg = document.querySelector('.darken-img');
     // remove current class
+    darkenImg.classList.remove("darken-img");
     current.classList.remove("current");
     // check if there's a sibling slide element
-    if (current.nextElementSibling) {
+    if (current.nextElementSibling && darkenImg.nextElementSibling) {
         // add current class to next sibling
+        darkenImg.nextElementSibling.classList.add("darken-img")
         current.nextElementSibling.classList.add("current")
     } else {
         // add current class to first slide
+        slides[0].classList.add("darken-img")
         slides[0].classList.add("current")
     }
+    setTimeout(() => darkenImg.classList.remove("darken-img"));
     setTimeout(() => current.classList.remove("current"));
 }
 
